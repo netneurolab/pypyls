8bd0c4df66913ec5d4920e33e0b3e35a9fd578f1
Ross Markello <rdm222@cornell.edu>
2017-03-12 23:58:28 -0400

    Start of parallelization
    Began to attempt to integrate parallelization for permutation testing; unclear
    if there is actually a gain in computation speed. Also added some utility
    functions (taken from plsgui toolbox) to normalize and cross-correlate. Added
    function to load nii data into data array.

--- pyls/compute.py
+++ pyls/compute.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
+import sys
 import multiprocessing as mp
 import numpy as np
 from sklearn.utils.extmath import randomized_svd
-from scipy.stats import zscore, sem
+from scipy.stats import sem
+from pyls.utils import xcorr
 
 
-def svd(X, Y, k, norm=True):
+def svd(X, Y, k):
     """
     Runs SVD on the covariance matrix of `X` and `Y`
 
@@ -19,28 +21,25 @@ def svd(X, Y, k, norm=True):
     Y : array (N x k)
     k : int
         rank of Y.T @ X matrix; determines # of singular vectors generated
-    norm : bool
-        whether to zscore X and Y prior to SVD
 
     Returns
     -------
     U, d, V : left singular vectors, singular values, right singular vectors
     """
 
-    if norm: X, Y = zscore(X), zscore(Y)
-    U, d, V = randomized_svd(Y.T @ X, n_components=k)
+    U, d, V = randomized_svd(xcorr(X, Y), n_components=k)
 
     return U, np.diag(d), V.T
 
 
-def procrustes(permuted, original, singular):
+def procrustes(original, permuted, singular):
     """
     Performs Procrustes rotation on `permuted` to align with `orig`
 
     Parameters
     ----------
-    permuted : array
     original : array
+    permuted : array
     singular : array
 
     Returns
@@ -48,7 +47,7 @@ def procrustes(permuted, original, singular):
     array : singular values of rotated `permuted` matrix
     """
 
-    N, O, P = np.linalg.svd(original.T @ permuted)
+    N, _, P = np.linalg.svd(original.T @ permuted)
     Q = N @ P
     resamp = permuted @ singular @ Q
 
@@ -68,6 +67,7 @@ def permute(X, Y, k, original, perms=1000, procs=1):
     k : int
         rank of Y.T @ X matrix
     original : array
+        U or V from original SVD for use in procrustes rotation
     perms : int
         number of permutations to run
     procs : int
@@ -78,22 +78,47 @@ def permute(X, Y, k, original, perms=1000, procs=1):
     array : distributions of singular values
     """
 
-    permuted_values = np.zeros((perms,k))
+    def callback(result):
+        permuted_values.append(result)
 
+    permuted_values = []
+    pool = mp.Pool(procs)
     for i in range(perms):
-        X_perm = np.random.permutation(X)
-        U, d, V = svd(X_perm, Y, k, norm=False)
+        pool.apply_async(single_perm,
+                         args=(X,Y,k,original),
+                         kwds={'seed':np.random.randint(4294967295)},
+                         callback=callback)
+    pool.close()
+    pool.join()
 
-        if len(U) < len(V): permuted = U
-        else: permuted = V
-
-        resamp, *rest = procrustes(permuted, original, d)
-        permuted_values[i] = np.sqrt((resamp**2).sum(axis=0))
+    permuted_values = np.array(permuted_values)
 
     return permuted_values
 
 
-def bootstrap(X, Y, k, U_orig, V_orig, boots=500, procs=1):
+def serial_perm(X, Y, k, original, perms=1000):
+    permuted_values = np.zeros((perms,k))
+
+    for n in range(perms):
+        permuted_values[n] = single_perm(X,Y,k,original)
+
+    return permuted_values
+
+
+def single_perm(data, behav, n_comp, orig, seed=None):
+    if seed is not None: np.random.seed(seed)
+    X_perm = np.random.permutation(data)
+    U, d, V = svd(X_perm, behav, n_comp)
+
+    if len(U) < len(V): permuted = U
+    else: permuted = V
+
+    resamp, *rest = procrustes(orig, permuted, d)
+
+    return np.sqrt((resamp**2).sum(axis=0))
+
+
+def bootstrap(X, Y, k, U_orig, V_orig, boots=500, procs=1, verbose=False):
     """
     Bootstrap `X`/`Y` (with replacement) and computes SE of singular values
 
@@ -109,6 +134,8 @@ def bootstrap(X, Y, k, U_orig, V_orig, boots=500, procs=1):
         number of boostraps to run
     procs : int
         function will multiprocess bootstraps for potential speed-up
+    verbose: bool
+        whether to print occasional progress messages
 
     Returns
     -------
@@ -120,11 +147,12 @@ def bootstrap(X, Y, k, U_orig, V_orig, boots=500, procs=1):
     I = np.identity(k)
 
     for i in range(boots):
+        if verbose and i % 100 == 0: print("Bootstrap {}".format(str(i)))
         inds = np.random.choice(np.arange(len(X)),size=len(X),replace=True)
         X_boot, Y_boot = X[inds], Y[inds]
         U, d, V = svd(X_boot, Y_boot, k, norm=True)
 
-        U_boot[:,:,i], Q = procrustes(U, U_orig, I)
+        U_boot[:,:,i], Q = procrustes(U_orig, U, I)
         V_boot[:,:,i] = V @ Q
 
     return U_boot, V_boot
@@ -148,7 +176,7 @@ def perm_sig(permuted_svalues, orig_svalues):
     """
 
     pvals = np.zeros(len(orig_svalues))
-    n_perms = len(permuted_svalues.shape)
+    n_perms = len(permuted_svalues)
 
     for i in range(len(pvals)):
         top_of_dist = np.where(permuted_svalues[:,i]>orig_svalues[i,i])[0]
--- pyls/utils.py
+++ pyls/utils.py
@@ -1,7 +1,106 @@
 #!/usr/bin/env python
 
 import glob
+from collections import Counter
 import numpy as np
+import nibabel as nib
+
+
+def flatten_niis(fnames,thresh=0.2):
+    """
+    Loads nii files in `fnames`, flattens, and removes non-zero voxels
+
+    Parameters
+    ----------
+    fnames : list (N)
+        list of nii files to be loaded
+    thresh : float
+        determines # of participants that must have activity in voxel for it
+        to be kept in final array
+
+    Returns
+    -------
+    array: N x non-zero voxels
+    """
+
+    # get some information on the data
+    cutoff = np.ceil(thresh*len(fnames))
+    x, y, z = nib.load(fnames[0]).shape
+    all_data = np.zeros((len(fnames),x*y*z))
+
+    # load in data
+    for n, f in enumerate(fnames):
+        temp = nib.load(f).get_data().flatten()
+        all_data[n] = temp
+
+    # get non-zero voxels
+    nz = np.array(Counter(np.where(all_data!=0)[1]).most_common())
+    all_data = all_data[:,nz[np.where(nz[:,1]>cutoff)[0],0]]
+
+    return all_data
+
+
+def xcorr(X, Y):
+    """
+    Calculates the cross correlation of `X` and `Y`
+
+    Parameters
+    ----------
+    X : array
+        data array
+    Y : array
+        behavior array
+
+    Returns
+    -------
+    array : cross-correlation of `X` and `Y`
+    """
+
+    avg,  stdev  = X.mean(axis=0), X.std(axis=0)
+    davg, dstdev = Y.mean(axis=0),  Y.std(axis=0)
+
+    checknan = np.where(stdev==0)
+    if checknan[0].size > 0:
+        X[checknan], avg[checknan[1]], stdev[checknan[1]] = 0, 0, 1
+
+    dchecknan = np.where(dstdev==0)
+    if dchecknan[0].size > 0:
+        Y[dchecknan], davg[dchecknan[1]], dstdev[dchecknan[1]] = 0, 0, 1
+
+    X, Y = (X-avg)/stdev, (Y-davg)/dstdev
+    xprod = (Y.T @ X)/(X.shape[0]-1)
+
+    return xprod
+
+
+def normalize(mat, dim=0):
+    """
+    Normalizes `origin` along dimension `dim`
+
+    Parameters
+    ----------
+    mat : array
+    dim : bool
+        Dimension for normalization
+
+    Returns
+    -------
+    array : normalized `mat`
+    """
+
+    normal_base = np.linalg.norm(mat,axis=dim,keepdims=True)
+    if dim == 1: normal_base = normal_base.T  # to ensure proper broadcasting
+
+    # to avoid DivideByZero errors
+    zero_items = np.where(normal_base==0)
+    normal_base[zero_items] = 1
+
+    # normalize and re-set zero_items
+    normal = mat/normal_base
+    normal[zero_items] = 0
+
+    return normal
+
 
 def get_rand_list(globform, seed=None):
     """
@@ -30,7 +129,7 @@ def get_rand_list(globform, seed=None):
 
     booll  = np.zeros(len(l)//2,dtype='bool')
     booll[np.random.choice(np.arange(len(l)//2),
-                           size=len(l)//(2/2),
+                           size=len(l)//4,
                            replace=False)] = True
 
     first  = np.array(l[slice(0,len(l),2)])
