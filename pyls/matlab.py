c6f1cb36bfa1e91937f851850b421652840766e8
Ross Markello <rossmarkello@gmail.com>
2018-03-08 17:29:01 -0500

    [TEST] Added matlab outputs for comparison
    And modified matlab.py code and functions

--- pyls/matlab.py
+++ pyls/matlab.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import scipy.io as sio
 
 
-def import_mean_centered_result(fname):
+def import_matlab_result(fname):
     """
     Imports ``fname`` PLS result from matlab
 
@@ -19,22 +20,19 @@ def import_mean_centered_result(fname):
     """
 
     result = sio.loadmat(fname)['result'][0, 0]
-    all_labels = ['method', 'is_struct', 'u', 's', 'v', 'usc', 'vsc',
-                  'num_subj_lst', 'num_conditions', 'perm_result',
-                  'boot_result', 'other_input', 'field_descrip']
-    perm_labels = ['num_perm', 'sp', 'sprob', 'permsamp', 'is_perm_splithalf']
-    boot_labels = ['num_boot', 'clim', 'num_LowVariability_behav_boots',
-                   'boot_type', 'nonrotated_boot', 'usc2', 'orig_usc', 'ulusc',
-                   'llusc', 'ulusc_adj', 'llusc_adj', 'prop', 'distrib',
-                   'bootsamp', 'compare_u', 'u_se', 'zero_u_se']
-    other_labels = ['meancentering_type', 'corrmode']
 
-    result = {all_labels[n]: value for n, value in enumerate(result)}
-    result['perm_result'] = {perm_labels[n]: value for n, value in
-                             enumerate(result['perm_result'][0, 0])}
-    result['boot_result'] = {boot_labels[n]: value for n, value in
-                             enumerate(result['boot_result'][0, 0])}
-    result['other_input'] = {other_labels[n]: value for n, value in
-                             enumerate(result['other_input'][0, 0])}
+    labels = list(result.dtype.fields.keys())
+    result = {labels[n]: value for n, value in enumerate(result)}
+
+    to_dict = ['perm_result', 'boot_result', 'other_input',  'perm_splithalf']
+    for attr in to_dict:
+        if result.get(attr, None) is not None:
+            labels = list(result[attr].dtype.fields.keys())
+            result[attr] = {labels[n]: np.squeeze(value) for n, value in
+                            enumerate(result[attr][0, 0])}
+
+    for key, val in result.items():
+        if isinstance(val, np.ndarray):
+            result[key] = np.squeeze(val)
 
     return result
