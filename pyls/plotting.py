a7a077517bd86593e5ade85c561a7a739fe2320e
Elizabeth DuPre <emd222@cornell.edu>
2018-03-09 16:57:58 -0500

    Patch, add docstrings

--- pyls/plotting.py
+++ pyls/plotting.py
@@ -1,46 +1,109 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
 
-def set_group_lvls(n_rpt, grp_lvls, n_grps):
+def set_group_lvls(n_conds, n_grps, grp_lvls=None):
     """
+    Derives a pandas data series of group labels for inclusion in
+    the created data frame used in plotting. Uses group labels
+    if provided by the user.
+
     Parameters
     ----------
+    n_conds
+        Number of conditions used in the analysis
+    n_grps
+        Number of groups used in the analysis
+    grp_lvls
+        (Optional) group labels
 
     Returns
     -------
+    pd.Series
+        A pandas series object of (optionally user-provided) group
+          labels, aligned to the input data structure.
+
     """
-    Group = []
-    for i in range(0, n_grps):
-        Group.extend([grp_lvls[i]]*n_rpt)
-    Group = pd.Series(Group, name='Group')
-    return Group
+    grping = []
+    if grp_lvls is None:
+        for i in range(n_grps):
+            grping += ["Group" + str(i)] * n_conds
+    else:
+        for i in range(n_grps):
+            grping.extend([grp_lvls[i]] * n_conds)
+    return pd.Series(grping, name='Group')
 
 
-def define_vars(result_dict, cond_lvls, grp_lvls):
+def set_cond_lvls(n_conds, n_grps, cond_lvls=None):
     """
+    Derives a pandas data series of condition labels for inclusion in
+    the created data frame used in plotting. Uses condition labels
+    if provided by the user.
+
     Parameters
     ----------
+    n_rpt
+        Number of times to repeat each group. This is usually
+          equal to the number of conditions.
+    n_grps
+        Number of groups used in the analysis
+    cond_lvls
+        (Optional) condition labels
 
     Returns
     -------
+    pd.Series
+        A pandas series object of (optionally user-provided) condition
+          labels, aligned to the input data structure.
+
+    """
+    if cond_lvls is None:
+        cond_lvls = ["Condition" + str(i) for i in range(n_conds)] * n_grps
+    else:
+        cond_lvls = cond_lvls * n_grps
+
+    return pd.Series(cond_lvls, name='Condition')
+
+
+def define_vars(result_dict, cond_lvls=None, grp_lvls=None):
+    """
+    Define a pandas data frame for plotting. Uses the result dictionary
+    returned by PLS, as well as user-supplied condition and group label(s),
+    if supplied.
+
+    Parameters
+    ----------
+    result_dict
+        The PLS result dictionary
+    cond_lvls
+        (Optional) condition labels
+    grp_lvls
+        (Optional) group labels
+
+    Returns
+    -------
+    pd.DataFrame
+        A pandas series with derived estimates (and upper- and lower-
+          estimated error) for all significant latent variables
     """
     estimate = result_dict['boot_result']['orig_usc']
     ul = result_dict['boot_result']['ulusc']
     ll = result_dict['boot_result']['llusc']
     n_grps = result_dict['num_subj_lst'].shape[1]
-    signif = result_dict['perm_result']['sprob']
+    n_conds = int(estimate.shape[1]/n_grps)
 
+    signif = result_dict['perm_result']['sprob']
     mask = signif < 0.05
     sigLV = mask.T[0]
     sigEstimate = estimate[sigLV].T
     sigUL = ul[sigLV].T
     sigLL = ll[sigLV].T
-    n_rpt = int(estimate.shape[1]/n_grps)
 
-    grp = set_group_lvls(n_rpt, grp_lvls, n_grps)
-    cond = pd.Series(cond_lvls * n_grps, name='Condition')
+    cond = set_cond_lvls(n_conds, n_grps, cond_lvls=grp_lvls)
+    grp = set_group_lvls(n_conds, n_grps, grp_lvls=grp_lvls)
 
     num_sig = sum(sigLV) + 1  # for 1-based indexing in plots
     colnames = []
@@ -50,17 +113,28 @@ def define_vars(result_dict, cond_lvls, grp_lvls):
 
     df = pd.DataFrame(np.hstack((sigEstimate, sigUL, sigLL)), columns=colnames)
     df = pd.concat([df, cond, grp], axis=1)
-
     return df
 
 
-def rearrange_df(df, plot_order):
+def rearrange_df(df, plot_order=None):
     """
+    In examining plots, users may wish to rearrange the order
+    in which conditions are presented in order to ease visual
+    interpretation. This function reorders the dataframe as
+    desired
+
     Parameters
     ----------
+    df
+        A pandas DataFrame containing condition, group labels and
+          PLS results for significant latent variables
+    (Optional) plot_order
+        User-defined order in which to plot conditions
 
     Returns
     -------
+    pd.DataFrame
+        A pandas DataFrame with re-ordered conditions
     """
     sorter_idx = dict(zip(plot_order, range(len(plot_order))))
     df['Cond_Arrange'] = df['Condition'].map(sorter_idx)
@@ -68,14 +142,25 @@ def rearrange_df(df, plot_order):
     return df.drop(columns=['Cond_Arrange'])
 
 
-def plot(df, idx, num_sig):
+def plot_bargraphs(df, idx):
     """
+    Plots bargraphs for each of the significant latent variables
+    in a provided PLS results data frame.
+
     Parameters
     ----------
+    df
+        A pandas DataFrame containing condition, group labels and
+          PLS results for significant latent variables
+    idx
+        Index of desired (significant) latent variable to plot
 
     Returns
     -------
+    matplotlib Axes obj
+        A matplotlib axes object for saving or modifying
     """
+    num_sig = (len(df.columns) - 2) // 3
     UL_num = idx + num_sig
     LL_num = idx + (num_sig * num_sig)
     ax = sns.barplot(x="Group", y=df[df.columns[idx]], hue="Condition", data=df,
@@ -83,8 +168,8 @@ def plot(df, idx, num_sig):
     ax.legend(bbox_to_anchor=(1.1, 1.05))
     x = [r.get_x() for r in ax.patches]
     nx = np.sort(x)
+    abs_err = np.abs([df[df.columns[LL_num]].get_values(),
+                      df[df.columns[UL_num]].get_values()] - df[df.columns[idx]].get_values())
     ax.errorbar(x=nx + (np.diff(nx).min() / 2),
-                y=df[df.columns[idx]], fmt='none',
-                yerr=np.abs(df[[df[df.columns[UL_num]],
-                                df[df.columns[LL_num]]]].get_values().T - df.Estimate_LV1.get_values()))
+                y=df[df.columns[idx]], fmt='none', yerr=abs_err)
     return ax
