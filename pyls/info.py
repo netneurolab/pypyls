80a2865f237dbe63a997db2b8abf7fac8ff0efb6
Ross Markello <rossmarkello@gmail.com>
2018-05-15 22:46:13 -0400

    [DOC] Added necessary setup.py info

--- pyls/info.py
+++ pyls/info.py
@@ -2,10 +2,14 @@ __version__ = '0.0.1'
 
 NAME = 'pyls'
 MAINTAINER = 'Ross Markello'
+EMAIL = 'rossmarkello@gmail.com'
 VERSION = __version__
 LICENSE = 'MIT'
-DESCRIPTION = 'A pythonic PLSC toolbox'
-DOWNLOAD_URL = 'http://github.com/rmarkello/pyls'
+DESCRIPTION = ('A toolbox for performing multivariate decomposition analyses')
+LONG_DESCRIPTION = ('')
+URL = 'http://github.com/rmarkello/pyls'
+DOWNLOAD_URL = ('https://github.com/rmarkello/{name}/archive/{ver}.tar.gz'
+                .format(name=NAME, ver=__version__))
 
 INSTALL_REQUIRES = [
     'numpy',
