5a12c1ed452b156f6c05b0ed590013088e83621e
Ross Markello <rossmarkello@gmail.com>
2018-12-05 14:09:37 -0500

    [ENH] Adds IO for PLSResults to HDF5 files

--- pyls/__init__.py
+++ pyls/__init__.py
@@ -3,7 +3,8 @@
 __all__ = [
     '__author__', '__description__', '__email__', '__license__',
     '__maintainer__', '__packagename__', '__url__', '__version__',
-    'behavioral_pls', 'meancentered_pls', 'import_matlab_result', 'PLSResults'
+    'behavioral_pls', 'meancentered_pls', 'import_matlab_result',
+    'PLSResults', 'save_results', 'load_results'
 ]
 
 from .info import (
@@ -18,8 +19,9 @@ from .info import (
 )
 
 try:
-    from .types import (behavioral_pls, meancentered_pls)
+    from .io import load_results, save_results
     from .matlab import import_matlab_result
     from .structures import PLSResults
+    from .types import (behavioral_pls, meancentered_pls)
 except ImportError:
     pass
