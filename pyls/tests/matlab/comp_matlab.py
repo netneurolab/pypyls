e2bafdd407c18406753bd2769723ae92e8b8c2f7
Ross Markello <rossmarkello@gmail.com>
2018-12-05 16:44:58 -0500

    [FIX] Fixes matlab comparison code

--- pyls/tests/matlab/comp_matlab.py
+++ pyls/tests/matlab/comp_matlab.py
@@ -12,35 +12,44 @@ EXAMPLES = [
 ]
 
 
-def assert_num_equiv(python, matlab, atol=1e-4):
-    # signs may be flipped so just take difference of absolute values
-    diff = np.abs(python) - np.abs(matlab)
+def assert_num_equiv(a, b, atol=1e-5, drop_last=True):
+    # signs may be flipped so adjust accordingly
+    flip = 1 * np.all(np.sign(b / a) == 1, axis=0, keepdims=True)
+    flip[flip == 0] = -1
+    diff = a - (b * flip)
 
     # the last LV is always screwed up so ignore it
-    if diff.ndim > 1:
-        diff = diff[:, :-1]
-    else:
-        diff = diff[:-1]
+    if drop_last:
+        if diff.ndim > 1:
+            diff = diff[:, :-1]
+        else:
+            diff = diff[:-1]
 
     assert np.allclose(diff, 0, atol=atol)
 
 
-def assert_func_equiv(a, b, corr=0.99):
+def assert_func_equiv(a, b, corr=0.99, drop_last=True):
     if a.ndim > 1:
-        corrs = np.array([np.corrcoef(a[:, i], b[:, i])[0, 1] for i in
-                          range(a.shape[-1] - 1)])
+        corrs = pyls.compute.efficient_corr(a, b)
+        if drop_last:
+            corrs = corrs[:-1]
     else:
-        corrs = np.corrcoef(a[:-1], b[:-1])[0, 1]
+        if drop_last:
+            a, b = a[:-1], b[:-1]
+        corrs = np.corrcoef(a, b)[0, 1]
     assert np.all(np.abs(corrs) > corr)
 
 
-def assert_pvals_equiv(a, b, alpha=0.05):
-    assert np.all((a[:-1] < alpha) == (b[:-1] < alpha))
+def assert_pvals_equiv(a, b, alpha=0.05, drop_last=True):
+    if drop_last:
+        a, b = a[:-1], b[:-1]
+    assert np.all((a < alpha) == (b < alpha))
 
 
 def make_comparison(fname, corr=0.99, alpha=0.05):
     # load matlab result
     fname = resource_filename('pyls', 'tests/data/{}'.format(fname))
+    drop_last = True if 'mpls' in fname else False
     matlab = pyls.matlab.import_matlab_result(fname)
 
     # fix n_split default (if not specified in matlab assume 0)
@@ -52,25 +61,33 @@ def make_comparison(fname, corr=0.99, alpha=0.05):
     python = fcn(**matlab.inputs, seed=1234)
 
     # check top-level results attributes for numerical equivalence
-    assert np.all([assert_num_equiv(python[k], matlab[k])[0]
-                   for k in python.keys() if isinstance(k, np.ndarray)])
+    for k in python.keys():
+        if isinstance(k, np.ndarray):
+            assert_num_equiv(python[k], matlab[k], drop_last=drop_last)
 
     # check pvals for functional equivalence
     if matlab.get('permres', {}).get('pvals') is not None:
-        assert_func_equiv(python.permres.pvals, matlab.permres.pvals, corr)
-        assert_pvals_equiv(python.permres.pvals, matlab.permres.pvals, alpha)
+        assert_func_equiv(python.permres.pvals, matlab.permres.pvals, corr,
+                          drop_last=drop_last)
+        assert_pvals_equiv(python.permres.pvals, matlab.permres.pvals, alpha,
+                           drop_last=drop_last)
 
     # check bootstraps for functional equivalence
     if matlab.get('bootres', {}).get('bootstrapratios') is not None:
         assert_func_equiv(python.bootres.bootstrapratios,
                           matlab.bootres.bootstrapratios,
-                          corr)
+                          corr, drop_last=drop_last)
 
     # check splitcorr for functional equivalence
     if matlab.get('splitres', {}).get('ucorr') is not None:
         # lenient functional equivalence
         for k in ['ucorr', 'vcorr']:
-            assert assert_num_equiv(python.splitres[k], matlab.splitres[k],
-                                    atol=0.1)
+            assert_func_equiv(python.splitres[k], matlab.splitres[k], corr,
+                              drop_last=drop_last)
+        # only consider the splithalf pvalues of the permuted LVs that are sig
+        # these are the only ones that we would consider, functionally speaking
         for k in ['ucorr_pvals', 'vcorr_pvals']:
-            assert_pvals_equiv(python.splitres[k], matlab.splitres[k], alpha)
+            pk = python.permres.pvals < alpha
+            mk = matlab.permres.pvals < alpha
+            assert_pvals_equiv(python.splitres[k][pk], matlab.splitres[k][mk],
+                               alpha, drop_last=False)
