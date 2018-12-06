d522ee0561d44d85db911f123f1144725d525fe1
Ross Markello <rossmarkello@gmail.com>
2018-12-06 10:42:37 -0500

    [TEST] Adds tests for efficient_corr()

--- pyls/tests/test_compute.py
+++ pyls/tests/test_compute.py
@@ -40,3 +40,19 @@ def test_xcorr():
         pyls.compute.xcorr(X[:, 0], Y[:, 0])
     with pytest.raises(ValueError):
         pyls.compute.xcorr(X[0:10], Y)
+
+
+def test_efficient_corr():
+    x, y = rs.rand(100), rs.rand(100, 10)
+    assert pyls.compute.efficient_corr(x, y).shape == (10,)
+    x = rs.rand(100, 10)
+    assert pyls.compute.efficient_corr(x, y).shape == (10,)
+
+    x = rs.rand(100, 2)
+    with pytest.raises(ValueError):
+        pyls.compute.efficient_corr(x, y)
+
+    x, y = np.ones((100, 2)), np.ones((100, 2)) * 5
+    x[50:, 0], y[50:, 0] = 2, 6
+    x[50:, 1], y[50:, 1] = 2, 4
+    assert np.allclose(pyls.compute.efficient_corr(x, y), np.array([1., -1.]))
